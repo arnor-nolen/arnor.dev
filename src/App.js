a7c764f661ec7713ff1c1dbf274a8094a115c215
host-ru <andrey1gorbulin@gmail.com>
2021-05-28 00:29:14 +0300

    Changed the logo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import logo from "./logo.svg";
+import { ReactComponent as Logo } from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Logo className="App-logo" />
         <p>Website is under construction, check it out later!</p>
       </header>
     </div>

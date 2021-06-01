022c4ddf4f3e5f2ed97cd844a9a2ba7680205b5a
host-ru <andrey1gorbulin@gmail.com>
2021-06-01 22:45:26 +0300

    Added landing page, my projects

--- src/App.js
+++ src/App.js
@@ -1,14 +1,64 @@
 import React from "react";
 import { ReactComponent as Logo } from "./logo.svg";
+import "react-awesome-button/dist/styles.css";
 import "./App.css";
+import { useQuery } from "@apollo/client";
+import { loader } from "graphql.macro";
+import { AwesomeButton } from "react-awesome-button";
+import { Link } from "react-scroll";
+
+const query = loader("./queries/fetchProjects.graphql");
 
 function App() {
+  const { loading, error, data } = useQuery(query);
+  let projectsMock = [];
+  for (let i = 0; i < 6; i++) {
+    projectsMock.push(
+      <div className="Project-cell" key={i}>
+        <div>Loading...</div>
+      </div>
+    );
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <Logo className="App-logo" />
-        <p>Website is under construction, check it out later!</p>
+        <Logo className="Author-pic" />
+        <div className="Author-name">Arnor Nolen</div>
+        <div className="Author-desc">software engineer from Finland</div>
+        <div className="Author-project-link">
+          <Link
+            to="my-projects"
+            spy={true}
+            smooth="easeInOutCubic"
+            duration={500}
+          >
+            <AwesomeButton type="primary" size="large">
+              Go to my projects
+            </AwesomeButton>
+          </Link>
+        </div>
       </header>
+      <section className="Projects" id="my-projects">
+        <div className="title">My projects</div>
+        <div className="Projects-grid">
+          {loading || error
+            ? projectsMock
+            : data.githubInfo.pinnedItems.nodes.map((x, i) => (
+                <div className="Project-cell" key={i}>
+                  <div className="Project-name">
+                    <a href={x.url}>{x.name}</a>
+                  </div>
+                  <div className="Project-desc">{x.description}</div>
+                  <div
+                    className="Project-language"
+                    style={{ color: x.primaryLanguage.color }}
+                  >
+                    Written in {x.primaryLanguage.name}
+                  </div>
+                </div>
+              ))}
+        </div>
+      </section>
     </div>
   );
 }
